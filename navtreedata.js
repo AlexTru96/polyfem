533b20d3361d1fdd6ec7a6a3aea39758b09109a9
AlexTru96 <77944734+AlexTru96@users.noreply.github.com>
2022-09-13 16:12:05 +0000

    Deploying to gh-pages from @ AlexTru96/polyfem@b25aa0b299290973c2326c4eb1c3c1913565640f 🚀

---
+++ navtreedata.js
@@ -0,0 +1,110 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PolyFEM", "index.html", [
+    [ "Code Structure", "index.html#autotoc_md1", [
+      [ "Finite Element Assembler", "index.html#autotoc_md2", null ],
+      [ "Finite Element Bases", "index.html#autotoc_md3", null ],
+      [ "Mesh data structure, Input/Output, and Navigation", "index.html#autotoc_md4", null ],
+      [ "Quadrature Rules", "index.html#autotoc_md5", null ],
+      [ "Time Integration", "index.html#autotoc_md6", null ],
+      [ "Collection of PDEs", "index.html#autotoc_md7", null ],
+      [ "Automatic Code Generation", "index.html#autotoc_md8", null ],
+      [ "Utilities", "index.html#autotoc_md9", null ]
+    ] ],
+    [ "Building PolyFEM as a stand-alone executable", "index.html#autotoc_md10", null ],
+    [ "Building PolyFEM as a static library", "index.html#autotoc_md11", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_a_l_n_l_problem_8cpp.html",
+"_mass_matrix_assembler_8cpp.html#ac5aceb98894ff82070f5ec3698f7b827",
+"_polygonal_basis3d_8hpp_source.html",
+"auto__p__bases_8hpp.html#acb852da43087916bb4919c07218b4540",
+"classpolyfem_1_1_lame_parameters.html#a2a57d1ca9744a7335d160548da60ed20",
+"classpolyfem_1_1_state.html#aa8c23a211e3b4f0fa6c502ef04038442",
+"classpolyfem_1_1assembler_1_1_assembly_values.html#ae47177bceda101096daacf64afd9b6a1",
+"classpolyfem_1_1assembler_1_1_generic_tensor_problem.html#ad8b80eda6699ba7c77ad9d1e05d489b1",
+"classpolyfem_1_1assembler_1_1_n_l_assembler.html#ac4e11d6258f994d2abd70957b6e3163a",
+"classpolyfem_1_1assembler_1_1_saint_venant_elasticity.html#a5bc6fe6f6302e84c761917fcb06bc51a",
+"classpolyfem_1_1basis_1_1_r_b_f_with_linear.html#ab60f858eefdd193a7e9277f8578e94b2",
+"classpolyfem_1_1mesh_1_1_c_mesh3_d.html#aa27534127474beed655235694ca37e7d",
+"classpolyfem_1_1mesh_1_1_mesh.html#af0424129f0b565f3f3b1743ee3ccffbe",
+"classpolyfem_1_1mesh_1_1_mesh_nodes.html#a8e222f339075e91de499bb7e5a8c11e5",
+"classpolyfem_1_1mesh_1_1_n_c_mesh3_d.html#a17fbbd159d8c4e9d3da70d50cf98a8c1",
+"classpolyfem_1_1mesh_1_1_obstacle.html#a2517abcf6fae9e92a6ddbdf7884923bd",
+"classpolyfem_1_1problem_1_1_double_torsion_elastic_problem.html#aa4a8db7edbf92f2cac0d2dddaedbb606",
+"classpolyfem_1_1problem_1_1_kernel_problem.html#a8b77cc772abec99b58ca9b80d55cc0a0",
+"classpolyfem_1_1problem_1_1_problem_with_solution.html#a7a2e8c1d3f156d7e16e2fe9fa52fd891",
+"classpolyfem_1_1problem_1_1_two_spheres.html#a4c03650c80c9e6c05fce0b0e56d1e3a0",
+"classpolyfem_1_1solver_1_1_n_l_problem.html#a547816d169bf851dfb8b24d13712a7a2",
+"classpolyfem_1_1solver_1_1_transient_navier_stokes_solver.html#a5539428f41d1e8549b2618aed7581b7f",
+"classpolyfem_1_1utils_1_1_expression_value.html#a76dc675f0165ead06fae6a1b4cd3510f",
+"classpolyfem_1_1utils_1_1_sphere_selection.html",
+"functions_vars_p.html",
+"namespacepolyfem_1_1mesh.html#a1786d2b6b9b3bda8c6667456f9b1f5caa77b3ac06c59fd2083e1ee4874c433832",
+"p__bases_8py.html#a98aea95f6d75db01c5d555965f8000fa",
+"structpolyfem_1_1mesh_1_1_face.html#a2dbcaa37b1e6f922df920ac1b6205574",
+"structpolyfem_1_1mesh_1_1_n_c_mesh3_d_1_1nc_elem.html#addf5cd146276db4d94d877919b1c6e03"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/typedefs_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['json_0',['json',['../namespacepolyfem.html#a77fa8fe96825827f2af726fcf6405d15',1,'polyfem']]]
+];
